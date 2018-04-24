2bf757f7a798262e481b435569cddae8ffd7e830
Junbin Kwon <jkwon2@my.westga.edu>
2018-04-24 17:05:13 -0400

    Project4 Strated and made scene

---
+++ src/edu/westga/cs1302/wordCloud/Main.java
@@ -0,0 +1,29 @@
+package edu.westga.cs1302.wordCloud;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("view/wordCloudGui.fxml"));
+			AnchorPane pane = loader.load();
+			Scene scene = new Scene(pane);
+			primaryStage.setTitle("A Word Game by Junbin Kwon");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
